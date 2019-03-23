f82b36a11fee724c56b5babed8a28d100eaa392c
Nick D <nicholasdavies192@gmail.com>
2019-03-23 20:21:35 +0000

    Initial Commit.

---
+++ DiscordCore.App/DiscordCore.cs
@@ -0,0 +1,43 @@
+﻿using DiscordCore.Configuration;
+using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiscordCore.App
+{
+    public class DiscordCoreApp
+    {
+        //private static PluginManager manager;
+
+        public void Start()
+        {
+            PluginManager manager = new PluginManager();
+            manager.Start();
+
+            //Thread worker = new Thread(PluginThread);
+            //worker.Start();
+            //while (true)
+            //{
+            //    string command = Console.ReadLine().ToLowerInvariant();
+            //    if (command == "quit")
+            //    {
+            //        break;
+            //    }
+            //    else if (command == "reload")
+            //    {
+            //        manager.Stop();
+            //        worker.Abort();
+            //        worker = new Thread(PluginThread);
+            //        worker.Start();
+            //    }
+            //}
+        }
+
+        //private static void PluginThread()
+        //{
+        //    manager = new PluginManager();
+        //    manager.Start();
+        //}
+    }
+}
